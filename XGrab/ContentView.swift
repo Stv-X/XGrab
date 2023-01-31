77410883151052e5a2aa44468b53f81c360ea928
Stv.X <xsm_2001@163.com>
2023-01-31 11:20:16 +0800

    Initial Commit

---
+++ XGrab/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XGrab
+//
+//  Created by 徐嗣苗 on 2023/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XGrab/XGrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  XGrabApp.swift
+//  XGrab
+//
+//  Created by 徐嗣苗 on 2023/1/31.
+//
+
+import SwiftUI
+
+@main
+struct XGrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
